5ab3761f93d529db7921f268b71772a438411c44
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-02 06:15:36 +0200

    Update

--- ftp-export.sh
+++ ftp-export.sh
@@ -2,34 +2,9 @@
 
 WORKING_DIR="$(pwd)"
 
-output() {
-  local MESSAGE="${1}"
-  local COLOR="${2}"
-
-  if [ -z "${COLOR}" ]; then
-    COLOR=2
-  fi
-
-  echo -e "$(tput setaf ${COLOR})${MESSAGE}$(tput sgr 0)"
-}
-
-output_warning() {
-  local MESSAGE="${1}"
-  local COLOR=3
-
-  output "${MESSAGE}" "${COLOR}"
-}
-
-output_error() {
-  local MESSAGE="${1}"
-  local COLOR=1
-
-  >&2 output "${MESSAGE}" "${COLOR}"
-}
-
 help() {
   cat << EOF
-ftp-export: Usage: ftp-export <EXPORTED_FILES> <HOSTNAME> <USERNAME> <PASSWORD> <BACKUP_PATH> [REMOTE_DIRECTORY]
+ftp-export: Usage: ftp-export <SOURCE> <HOSTNAME> <USERNAME> <PASSWORD> <DESTINATION> [REMOTE_DIRECTORY]
 EOF
 
   exit 1
@@ -40,13 +15,81 @@ if [ "${1}" == "-h" ] || [ "${1}" == "--help" ]; then
 fi
 
 unknown_command() {
-  output_error "ftp-export: Unknown command. See 'ftp-export --help'"
+  echo "ftp-export: Unknown command. See 'ftp-export --help'"
 
   exit 1
 }
 
-if [ "${#}" -lt 2 ] || [ "${#}" -gt 3 ]; then
+if [ "${#}" -lt 5 ] || [ "${#}" -gt 6 ]; then
   unknown_command
 fi
+
+SOURCE="${1}"
+
+if [ ! -d "${SOURCE}" ]; then
+  echo "svn-export: Source directory doesn't exist: ${SOURCE}"
+
+  exit 1
+fi
+
+mkdir -p "${SOURCE}"
+
+cd "${SOURCE}"
+
+SOURCE="$(pwd)"
+
+cd "${WORKING_DIR}"
+
+DESTINATION="${5}"
+
+if [ -d "${DESTINATION}" ]; then
+  echo "svn-export: Destination directory already exists: ${DESTINATION}"
+
+  exit 1
+fi
+
+mkdir -p "${DESTINATION}"
+
+cd "${DESTINATION}"
+
+DESTINATION="$(pwd)"
+
+REMOTE_DIRECTORY=""
+
+if [ "${#}" -gt 5 ]; then
+  REMOTE_DIRECTORY="${6}"
+
+  REMOTE_DIRECTORY_CHARACTERS_COUNT=$((${#REMOTE_DIRECTORY} - 1))
+
+  if [ "${REMOTE_DIRECTORY:${REMOTE_DIRECTORY_CHARACTERS_COUNT}:1}" != "/" ]; then
+    REMOTE_DIRECTORY="${REMOTE_DIRECTORY}/";
+  fi
+fi
+
+HOSTNAME="${2}"
+USERNAME="${3}"
+PASSWORD="${4}"
+
+for FILE in $(find "${SOURCE}" -type f); do
+  RELATIVE_PATH="${FILE/${SOURCE}}"
+
+  if [ "${RELATIVE_PATH:0:1}" == "/" ]; then
+    RELATIVE_PATH="$(echo ${RELATIVE_PATH} | cut -c 2-)"
+  fi
+
+  URL="ftp://${USERNAME}:${PASSWORD}@${HOSTNAME}/${REMOTE_DIRECTORY}${RELATIVE_PATH}"
+
+  #echo $(wget -S --spider "${URL}" -O /dev/null)
+
+  DIRECTORY="$(dirname RELATIVE_PATH)"
+
+  if [ ! -d "${DIRECTORY}" ]; then
+    mkdir -p "${DIRECTORY}"
+  fi
+
+  echo "ftp-export: Exporting file: ${RELATIVE_PATH}"
+
+  wget -q "${URL}" -O "${RELATIVE_PATH}"
+done
 
 cd "${WORKING_DIR}"
